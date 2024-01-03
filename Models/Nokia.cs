dd0fdee1373af44a6b5ec31a5a77dc559c7b3184
diegowoolley <154614603+diegowoolley@users.noreply.github.com>
2024-01-02 22:21:12 -0300

    Add files via upload

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,17 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero)
+        {
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+           nomeApp = "Instalando Aplicativo: Whatsapp";
+           Console.WriteLine(nomeApp);
+        }
+       
     }
 }
