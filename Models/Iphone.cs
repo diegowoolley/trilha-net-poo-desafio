672e240f178a897e63fc2a6285fc041e0b12b675
Diego Woolley <diegowoolley@hotmail.com>
2024-01-02 21:57:41 -0300

    desafio concluido

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,21 @@
+using System.Runtime.Serialization;
+
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero)
+        {
+        }
+       
+       
+        
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            nomeApp = "Instalando o Aplicativo: Telegram";
+            Console.WriteLine(nomeApp);
+        }
     }
 }
